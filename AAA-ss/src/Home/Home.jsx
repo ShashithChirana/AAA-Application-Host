78e5dc5264e1ddfa851bb627bfd8584979bb88da
Shashith Chirana <shashithchirana@gmail.com>
2025-03-09 19:55:51 +0530

    85th commit

--- AAA-ss/src/Home/Home.jsx
+++ AAA-ss/src/Home/Home.jsx
@@ -11,18 +11,17 @@ const Home = () => {
   };
 
   return (
+   <div className="frame">
     <div className="home-container">
-      <h1 className="home-heading">SECURE WRAPPER APPLICATION  </h1>
-      <img
-        src={myImage} 
-        alt="AAA Application"
-        className="home-image"
-      />
+      <h1 className="home-heading">SECURE WRAPPER   </h1>
+      <h2 className="home-heading2">APPLICATION</h2>
+     
       <div>
       <button className="home-button" onClick={handleGetStarted}>
         Get Started
       </button>
       </div>
+    </div>
     </div>
   );
 };
